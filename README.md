ccb2a551f48354cfd356ebc31a922ad125a683e2
stao7 <stao7@ucsc.edu>
2022-06-28 14:04:08 -0700

    P2

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# p2-simple-scene-starter
+# P2 Simple Scene Generation
+***A project that use 2D drawing to redraw "the inspiring image" by using javascript.***
+![alt](https://drive.google.com/file/d/16WspcrNxDh8lZg5F6iAJzINrxkNg86OY/view?usp=sharing)
+
+In my scene, I used randomness in the position and color of the flowers, the position of the trees and the shape of the mountain, because in the inspiring image, the picture locked in a position which give us a view of blurred flowers, trees and mountains, therefore what I was trying to do is after each roll, the train will be like it is in a new position where the views will be different because now the picture is locked in a different perspectives.
+
+Since there is a train, the reacitivity of the scene must include the train in it, therefore when I construct the train with the shapes, the x position values where tied with the mouseX position. Also, the train road will also move with the mouse, but in order to let it feels like it is moving, I made the road's x position mouseX divided by 2, therefore the road will be moving but slower than the train, and it will be more vivid.
+
+The effect I am trying to achieve is to use what is in the picture and simulate a scene that the train in the picture is moving and the view will be changing differently every reroll clicked. I believe I mostly achieved the goal, but because of it is 2D and the construction of the shapes will not perfectly performes what is in the picture, the scene is not perfect.
 
 Credit: Adam Smith; Asiiah Song
--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 /* exported setup, draw */
 let seed = 12345;
 
-const grassColor = "#e1ac4a";
-const skyColor = "#cdd8e6";
-const hillColor = "#1e273f";
+const grassColor = "#56e346";
+const skyColor = "#0bdbc0";
+const hillColor = "#ffffff";
 const treeColor = "#3d1803";
 const leaveColor = "#233610";
+const petalColor1 = '#e3e01b';
+const petalColor2 = '#ff05e6';
+const petalColor3 = '#ff0505';
+const flowerColor = '#284d34';
+const trainColor = '#ff0a0a';
+const roadColor = '#bfbfbf';
+const tireColor = '#000000';
+const woodColor = '#422200';
+const airColor = '#001442';
 const sunColor = [254,254,254,80]; // with opacity
 
 function preload() {
@@ -27,16 +36,26 @@ function draw() {
 
   fill(skyColor);
   rect(0, 0, width, height / 2);
-
+  
   // An example of making something respond to the mouse
   fill(...sunColor);
-  ellipse(mouseX,0,30,30);
-  ellipse(mouseX,0,50,50);
-  ellipse(mouseX,0,100,100);
-  ellipse(mouseX,0,200,200);
+  ellipse(0,0,30,30);
+  ellipse(0,0,50,50);
+  ellipse(0,0,100,100);
+  ellipse(0,0,200,200);
 
   fill(grassColor);
   rect(0, height / 2, width, height / 2);
+  fill(roadColor);
+  stroke(tireColor);
+  rect(-2, 250, 1000, 25);
+  stroke(woodColor);
+  strokeWeight(5);
+  for(let i = -40; i < 40; i++){
+    line(mouseX/2 + i* 20, 252, mouseX/2 + 10 + i* 20, 272); 
+  }
+  strokeWeight(2);
+  stroke(airColor);
 
   // An example of drawing an irregular polygon
   fill(hillColor);
@@ -46,7 +65,7 @@ function draw() {
   for (let i = 0; i < steps + 1; i++) {
     let x = (width * i) / steps;
     let y =
-      height / 2 - (random() * random() * random() * height) / 8 - height / 50;
+      height / 2*random() - (random() * random() * random() * height) / 8 - height / 50;
     vertex(x, y);
   }
   vertex(width, height / 2);
@@ -56,11 +75,34 @@ function draw() {
   for (let i = 0; i < trees; i++) {
     drawLtree();
   }
+  noStroke();
+  fill(tireColor);
+  circle(mouseX + 10, 250, 15);
+  circle(mouseX + 111, 250, 15);
+  circle(mouseX + 212, 250, 15);
+  circle(mouseX + 90, 250, 15);
+  circle(mouseX + 191, 250, 15);
+  circle(mouseX + 292, 250, 15);
+  fill(trainColor);
+  rect(mouseX, 200, 100, 50);
+  rect(mouseX + 101, 200, 100, 50);
+  rect(mouseX + 202, 200, 100, 50);
+  ellipse(mouseX + 300, 225, 100, 50);
+  fill(hillColor);
+  rect(mouseX + 15, 213, 25, 25);
+  rect(mouseX + 50, 213, 25, 25);
+  rect(mouseX + 116, 213, 25, 25);
+  rect(mouseX + 151, 213, 25, 25);
+  rect(mouseX + 217, 213, 25, 25);
+  rect(mouseX + 252, 213, 25, 25);
+  for (let i = 0; i < trees * 2; i++) {
+    drawFlower();
+  }
 
   // An example of recursively drawing an L-tree 
   function drawLtree() {
     let x = width * random();
-    let y = height/2 + height/8 * random();
+    let y = 200+ height/8 * random();
     let s = width/200 + (y - height/2)/2;
     let jitter = (mouseX - width/2) / width * 2 * Math.PI / 180;
     drawLtreeBranch(x, y, s, (-90 * Math.PI / 180) + jitter, 0, 5); // this angle points north (0 is east)
@@ -77,7 +119,7 @@ function draw() {
     let x2 = x1 + vx;
     let y2 = y1 + vy;
     line(x1, y1, x2, y2);
-
+  
     let new_s = s * 0.7;
     let new_max = max_limit + random();
     let new_branch_weight = branch_weight - 1;
@@ -104,7 +146,6 @@ function draw() {
             drawLeave(x2, y2, new_s, (35 * Math.PI / 180) + angle);
         }
     }
-
   }
 
   function drawLeave(x, y, s, angle) {
@@ -118,8 +159,61 @@ function draw() {
     let x2 = x1 + vx;
     let y2 = y1 + vy;
     line(x1, y1, x2, y2);
-    circle(x2, y2, 3);
-
+    circle(x2, y2, 5);
   }
+
+  
+  function drawFlower(){
+    let x = width * random();
+    let y = 300 + height/8 * random();
+    let s = width/200 + (y - height/2)/6;
+    let jitter = (mouseX - width/2) / width * 2 * Math.PI / 180;
+    stroke(flowerColor);
+    strokeWeight(4);
+    let v = p5.Vector.fromAngle((-90 * Math.PI / 180) + jitter, s);
+    let vx = v.x;
+    let vy = v.y; 
+    let x1 = x;
+    let y1 = y; 
+    let x2 = x1 + vx;
+    let y2 = y1 + vy;
+    line(x1, y1, x2, y2);
+    if (random() < 1/3) {
+      fill(petalColor1);
+    } else if (random() > 1/3) {
+      fill(petalColor2);
+    } else {
+      fill(petalColor3);
+    }
+    
+    noStroke();
+    line(x1, y1, x2, y2);
+    circle(x2, y2, 6);
+    circle(x2 - 6, y2, 10);
+    circle(x2 + 6, y2, 10);
+    circle(x2, y2 + 6, 10);
+    circle(x2, y2 - 6, 10);
+    stroke(flowerColor);
+    strokeWeight(3);
+
+    v = p5.Vector.fromAngle((60), s/2);
+    vx = v.x;
+    vy = v.y; 
+    x1 = x;
+    y1 = y; 
+    x2 = x1 + vx;
+    y2 = y1 + vy;
+    line(x1, y1, x2, y2);
+
+    v = p5.Vector.fromAngle((50), s/2);
+    vx = v.x;
+    vy = v.y; 
+    x1 = x;
+    y1 = y; 
+    x2 = x1 + vx;
+    y2 = y1 + vy;
+    line(x1, y1, x2, y2);
+  }
+  
 }
 
